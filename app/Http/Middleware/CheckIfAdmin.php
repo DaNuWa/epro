6b6d6b227fc19a08707c08cd738b705ab683e6ce
YOHAN <yohan@L0082-CHAMPSOFT.localdomain>
2022-11-12 12:11:26 +0530

    Admin middleware logic updated

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -42,7 +42,7 @@ class CheckIfAdmin
         if ($request->ajax() || $request->wantsJson()) {
             return response(trans('backpack::base.unauthorized'), 401);
         } else {
-            return redirect()->guest(backpack_url('login'));
+          abort(403,'You can\t access this page');
         }
     }
 
