4076e3b9d9fd39d7ddcd1f50b5d1cdfad3e8d640
YOHAN <yohan@L0082-CHAMPSOFT.localdomain>
2022-11-13 14:05:59 +0530

    insta link updated

--- resources/views/livewire/poster.blade.php
+++ resources/views/livewire/poster.blade.php
@@ -1,7 +1,6 @@
 <div class="bg-primary py-5">
   <div class="container py-5 text-light">
     <h1 class="display-1">
-    Service from a <br>fingertip </h1>
-   
+    Service from a fingertip </h1>
   </div>
 </div>
